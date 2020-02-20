4160ebbe19e6687d0d256cbaccf23d179ffab221
Alexander Ma <alexanderzma@hotmail.com>
2020-02-19 18:49:33 -0800

    Audio test

---
+++ Assets/Scenes/UIStuff/Scripts/PlayOnClickAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnClickAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public SetCursor setCursor;
+    public GameObject menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetMouseButtonDown(0) && setCursor.IsChanged() && !menu.activeSelf){
+            audioSource.Play();
+		}
+    }
+}
