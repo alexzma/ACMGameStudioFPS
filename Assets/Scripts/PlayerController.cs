9e02d94f857b522fe6c0e26f7a1032b28712a60c
valentinlagunes <38746232+valentinlagunes@users.noreply.github.com>
2020-02-29 16:09:33 -0800

    redid basic player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 1;
+    public float forward;
+    public float side;
+    public float jumpForce;
+    public Rigidbody playerRb;
+    public bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        forward = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * forward);        
+        side = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * Time.deltaTime * side);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
-    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
     public void Respawn()
     {
     }
